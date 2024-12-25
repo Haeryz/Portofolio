c99bc0289831815bc54c71339b1c6824ec461238
Haeryz <haeriz42069@gmail.com>
2024-12-25 18:49:14 +0700

    Refactor Home component styling and add animations; enhance Sidebar layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import './Home.scss'
 
 function Home() {
   return (
@@ -11,12 +11,12 @@ function Home() {
           <br />
           I&apos;m
           <img src={LogoTitle} alt="developer" />
-          lobodan
+          ariz
           <br />
           web developer
+        </h1>
           <h2>Frontend Developer / Javavascript expert</h2>
           <Link to={"/contact"} className='flat-button'>CONTACT ME</Link>
-        </h1>
       </div>
     </div>
   )
